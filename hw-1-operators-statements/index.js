712b105b369c604b038f40c464af90b624b64bf4
Artem_Lakatun <alakatuni@mail.ru>
2023-07-01 15:58:06 +0300

    add: add homework dir

---
+++ hw-1-operators-statements/index.js
@@ -0,0 +1,87 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+// function should return sum of two numbers
+const sum = (a, b) => {
+  // write your code here
+};
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+// function should return true if number is even or false is number is not even
+const isNumberEven = (num) => {
+  // write your code here
+};
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {string}
+ */
+// Write logic to compare two numbers and return which number is greater or they are equal
+// If num1 is greater than num2 return string 'num1 is the largest number'
+// if num2 is greater than num1 return string 'num2 is the largest number'
+// if num1 is equal to num2 return string 'num1 is equal to num2'
+const findLargestNumber = (num1, num2) => {
+  // write your code here
+};
+
+/**
+ * @param {number} side1
+ * @param {number} side2
+ * @param {number} side3
+ * @return {string}
+ */
+// function should return type of triangle
+// if triangle is equilateral return string 'Equilateral triangle'
+// if triangle is Scalene return string 'Scalene triangle'
+// if triangle is Isosceles return string 'Isosceles triangle'
+const findTriangleType = (side1, side2, side3) => {
+  // write your code here
+};
+
+/**
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+// function should return amount of days in month with string 'The Month has X days'
+// If wrong month number provided return string 'Invalid Month of value X'
+const findDaysInMonth = (month, year) => {
+  // write your code here
+}
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @param {string} operation
+ * @return {number | string}
+ */
+// function should return result of operation of num1 and num2 based on operation parameter.
+// If operation is invalid return string 'Invalid operation'
+const calculateResult = (num1, num2, operation) => {
+  // write your code here
+}
+
+/**
+ * @return {string}
+ */
+// Create multiply table multiplying all digits from 2 t0 9 on 1 - 10 and write result in string
+// Compare your result with string in test
+const getMultiplicationTable = () => {
+  // write your code here
+};
+
+module.exports = {
+  isNumberEven,
+  findLargestNumber,
+  findTriangleType,
+  findDaysInMonth,
+  sum,
+  calculateResult,
+  getMultiplicationTable,
+};
