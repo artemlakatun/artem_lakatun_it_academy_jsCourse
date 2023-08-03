bf1bff684712fa2325e64361df0aea6c9228638e
Artem_Lakatun <alakatuni@mail.ru>
2023-08-03 21:35:48 +0300

    feat: add passed homework 5

--- hw5-dom-table/index.js
+++ hw5-dom-table/index.js
@@ -2,5 +2,24 @@ fetch('https://dummyjson.com/products')
   .then(res => res.json())
   .then((data) => {
     console.log(data.products);
-    // Start here :)
+    const products = data.products;
+    const cards = document.querySelectorAll('.card');
+    products.forEach((product, index) => {
+        const card = cards[index];
+        if (card) {
+          const image = card.querySelector(".card-img-top");
+          const title = card.querySelector(".card-title");
+          const description = card.querySelector(".card-text");
+          const price = card.querySelector(".price");
+          const rating = card.querySelector('.rating');
+          const lastCardButton = card.querySelector('.btn');
+
+            image.src = product.images[0];
+            title.textContent = product.title;
+            description.textContent = product.description;
+            price.textContent = `Price: $${product.price}`;
+            rating.textContent = product.rating;
+            lastCardButton.textContent = 'Add to card';
+        }
+    })
   });
